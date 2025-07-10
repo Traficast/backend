eafb9d0c5fc81d0aeb35e9ca3f69d2466138c7ea
junhui <junhuilim@kakao.com>
2025-07-11 00:43:37 +0900

    ModelConfigRepository 인터페이스 생성

---
+++ src/main/java/com/traficast/repository/ModelConfigRepository.java
@@ -0,0 +1,27 @@
+package com.traficast.repository;
+
+import com.traficast.entity.ModelConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ModelConfigRepository extends JpaRepository<ModelConfig, Long> {
+
+    /**
+     * 현재 활성화된 모델 조회
+     */
+    Optional<ModelConfig> findTopByIsActiveTrueAndIsDeletedFalseOrderByActivationDateDesc();
+
+    /**
+     * 모델 타입별 조회
+     */
+    List<ModelConfig> findByModelTypeAndIsDeletedFalseOrderByCreatedAtDesc(ModelConfig.ModelType modelType);
+
+    /**
+     * 활성화된 모든 모델 조회
+     */
+    List<ModelConfig> findByIsActiveTrueAndIsDeletedFalseOrderByActivationDateDesc();
+}
